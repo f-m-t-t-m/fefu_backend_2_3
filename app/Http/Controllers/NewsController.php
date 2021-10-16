0bdec4df7698ef2ac380dc79326684e8905e71d2
Talkee <aglicheev.ao@gmail.com>
2021-10-16 23:03:30 +1000

    Lesson 4: Select only published news

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -8,17 +8,20 @@ use \App\Models\News;
 class NewsController extends Controller
 {
     public function getList() {
-        $news = News::query()->where('published_at', '<=', 'now')
-            ->orderBy('published_at', 'desc')->orderBy('id', 'desc')->paginate(5);
-
+        $news = News::query()->where('published_at', '<=', 'NOW')
+            ->where('is_published', '=', '1')
+            ->orderByDesc('published_at')
+            ->orderByDesc('id')->paginate(5);
         return view('news', ['news' => $news]);
     }
 
     public function getDetails(string $slug) {
         $news = News::query()->where('slug', $slug)
-            ->where('published_at', '<=', 'now')->first();
-        if ($news === null)
+            ->where('is_published', '=', '1')
+            ->where('published_at', '<=', 'NOW')->first();
+        if ($news === null) {
             abort(404);
+        }
         return view('news_item', ['news' => $news]);
     }
 }
