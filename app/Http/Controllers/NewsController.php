11ac9bce893a05e23157d276978630c94ed0486d
Talkee <aglicheev.ao@gmail.com>
2021-10-22 16:46:02 +1000

    Lesson 4: Fix select published news

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -9,7 +9,7 @@ class NewsController extends Controller
 {
     public function getList() {
         $news = News::query()->where('published_at', '<=', 'NOW')
-            ->where('is_published', '=', '1')
+            ->where('is_published', true)
             ->orderByDesc('published_at')
             ->orderByDesc('id')->paginate(5);
         return view('news', ['news' => $news]);
@@ -17,7 +17,7 @@ class NewsController extends Controller
 
     public function getDetails(string $slug) {
         $news = News::query()->where('slug', $slug)
-            ->where('is_published', '=', '1')
+            ->where('is_published', true)
             ->where('published_at', '<=', 'NOW')->first();
         if ($news === null) {
             abort(404);
