52980950d3ca2ba21db2ed5c449bd8ecc3b4e4ac
Alexander Aglicheev <73017769+f-m-t-t-m@users.noreply.github.com>
2021-11-01 23:55:42 +1000

    Lesson 7: Add RedirectFromOldSlug to global middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        RedirectFromOldSlug::class
     ];
 
     /**
@@ -37,8 +38,7 @@ class Kernel extends HttpKernel
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            RedirectFromOldSlug::class
+            \Illuminate\Routing\Middleware\SubstituteBindings::class
         ],
 
         'api' => [
